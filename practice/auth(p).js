de145513d1cb962bbf5edce728d1a29290607494
Hikko218 <heikor93@outlook.de>
2025-06-17 21:27:50 +0200

    ..

--- practice/auth(p).js
+++ practice/auth(p).js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const bcrypt = require('bcrypt');
 
-const usersPath= path.join(__dirname,'../data/users.json');
+const usersPath = path.join(__dirname,'../data/users.json');
 
 //user load function
 function loadUsers() {
@@ -18,3 +18,24 @@ function loadUsers() {
 function safeUsers(users){
     fs.writeFileSync(usersPath,JSON.stringify(users,null,2));
 }
+
+//Post user registration
+app.post('./registration', async (req,res) =>{
+    const{username, password} = req.body;
+    const users = loadUsers();
+
+    if(!username || !password)
+        return res.status(401).json({message:'username and password required'});
+
+    if (users.find(u => u.username === username))
+        return res.status(409).json({message:'username already exists'});
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    users.push({username, password: hashedPassword});
+
+    safeUsers();
+
+    res.status(201).json({message:'Registration successfull'})
+
+})
